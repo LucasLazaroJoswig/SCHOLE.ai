256fb85bf4f51dd9d1251ba89d38303a768a2112
Deivi Perdomo <deivi54357326@gmail.com>
2024-04-17 23:56:24 +0200

    Configurando la validación de correo

--- assets/php/servidor.php
+++ assets/php/servidor.php
@@ -26,7 +26,22 @@ if (isset($_REQUEST['peticion'])) {
             $datos['sql']=$sql;
             $datos['datos'] = BBDD_CTRLR::CRUD($sql, 'i');
             // Devuelvo a JS los datos codificados como JSON
-            echo json_encode($datos);  
+            echo json_encode($datos); 
+            $to = 'anamarvelperdomo@gmail.com';
+            $subject = 'Confirmación de correo electrónico';
+            $message = 'Haz clic en el siguiente enlace para confirmar tu correo electrónico: http://tusitio.com/confirmar-correo?token=abcd1234';
+            $headers = 'From: deivi54357326@gmail.com' . "\r\n" .
+                        'anamarvelperdomo@gmail.com' . "\r\n" .
+                        'X-Mailer: PHP/' . phpversion();
+
+            
+            if (mail($to, $subject, $message, $headers)){
+                echo "Correo enviado con éxito";
+            }else{
+                echo "Error al enviar correo";
+            }
+
+
             break;
 
 
